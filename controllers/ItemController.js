133011fc0a53180b22ccdf636a724e8760c72fd9
hivindu1234 <wad.hivindu@gmail.com>
2022-11-03 00:49:21 +0530

    orde related apis

---
+++ controllers/ItemController.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const apiResponse = require("../helpers/apiResponse");
+const uniqueID = require("../helpers/uniqueID");
+const ItemModel = require("../models/ItemsModel");
+
+const getItems = async (req, res) => {
+  try {
+    const Items = await ItemModel.find();
+    apiResponse.Success(res, "Items", { Items: Items });
+  } catch (err) {
+    console.error(err.message);
+    apiResponse.ServerError(res, "Server Error", { err: err });
+  }
+};
+
+const getItem = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const Item = await ItemModel.find({ _id: id });
+    apiResponse.Success(res, "Item", { Item: Item });
+  } catch (err) {
+    console.error(err.message);
+    apiResponse.ServerError(res, "Server Error", { err: err });
+  }
+};
+
+const createItem = async (req, res) => {
+    const Item = req.body;
+
+    const newItem = new ItemModel({ ...Item });
+
+    //generate Item id
+    const item_Id = await uniqueID.generateItemID();
+    newItem.item_id = item_Id;
+
+    console.log("Saved Item data", newItem);
+    try {
+        await newItem.save();
+
+        apiResponse.Success(res, "NewItem", { newItem: newItem });
+
+    } catch (err) {
+        console.error(err.message);
+        apiResponse.ServerError(res, "Server Error", { err: err });
+    }
+}
+
+
+const updateItem = async (req, res) => {
+    const { id } = req.params;
+    const { item_Id, name, description, is_wholesale, unit_price, image_url } = req.body;
+
+    const filter = { _id: id };
+    const update = {
+        item_Id: item_Id,
+        name: name,
+        description: description,
+        is_wholesale: is_wholesale,
+        unit_price: unit_price,
+        image_url: image_url,
+    };
+
+    try {
+
+        let data = await ItemModel.findOneAndUpdate(filter, update);
+        console.log(data);
+        apiResponse.Success(res, "Item Details Updated", { data: data });
+
+    } catch (error) {
+        apiResponse.ServerError(res, "Server Error", { err: error });
+    }
+}
+
+
+const deleteItem = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No Item with id: ${id}`);
+
+    await ItemModel.findByIdAndRemove(id);
+
+    apiResponse.Success(res, "Item Deleted", {});
+}
+
+module.exports = { getItems, getItem, deleteItem, createItem, updateItem };
--- helpers/uniqueID.js
+++ helpers/uniqueID.js
@@ -11,4 +11,25 @@ exports.generateID = async ()=>{
     return final;
 }
 
+exports.generateItemID = async ()=>{
+
+    //var id_1 = await uniqid.process();    /**create a 12 digit id with current time and some strings */
+    var id_2 = Math.floor(1000 + Math.random() * 9000); /**generate 4 digit random number  */
+    var id_3 = Math.floor(100 + Math.random() * 900);/**generate 3 digit random number  */
+
+    var final = "ITM" + id_2 + id_3;
+
+    return final;
+}
+
+exports.generateItemPackageID = async ()=>{
+
+    //var id_1 = await uniqid.process();    /**create a 12 digit id with current time and some strings */
+    var id_2 = Math.floor(1000 + Math.random() * 9000); /**generate 4 digit random number  */
+    var id_3 = Math.floor(100 + Math.random() * 900);/**generate 3 digit random number  */
+
+    var final = "ITMPKG" + id_2 + id_3;
+
+    return final;
+}
 
