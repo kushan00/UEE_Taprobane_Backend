a083faa2f1bf282d3df5065cc6b59b24831437e2
Kushan Dimantha <kushandimantha533@gmail.com>
2022-11-10 21:20:57 +0530

    forign , wholesale buyer complete

--- routes/DeliveryAddressRoutes.js
+++ routes/DeliveryAddressRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {getDeliveryAddress, getDeliveryAddresss,createDeliveryAddress,updateDeliveryAddress} = require("../Controllers/DeliveryAddressController");
+const {getDeliveryAddress, getDeliveryAddresss,createDeliveryAddress,updateDeliveryAddress,deleteDeliveryDetails} = require("../Controllers/DeliveryAddressController");
 
 
 router.post("/createDeliveryAddress",createDeliveryAddress);
 router.get("/getDeliveryAddresss",getDeliveryAddresss);
 router.get("/getDeliveryAddressById/:id",getDeliveryAddress);
 router.put("/updateDeliveryAddress/:id",updateDeliveryAddress);
+router.delete("/deleteDeliveryDetails/:id",deleteDeliveryDetails);
 
 
 
--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {getCard, getCards, createCard,updateCard} = require("../Controllers/CardController");
+const {getCard, getCards, createCard,updateCard,deleteCardDetails} = require("../Controllers/CardController");
 
 
 router.post("/createCard",createCard);
 router.get("/getCards",getCards);
 router.get("/getCardById/:id",getCard);
 router.put("/updateCard/:id",updateCard);
+router.delete("/deleteCardDetails/:id",deleteCardDetails);
 
 
 
