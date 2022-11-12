7c8f3a81439405bab4e570355f8ea6815ffa23b9
NethmiPrabodhika <prabodhikanethmi@gmail.com>
2022-11-12 21:24:47 +0530

    merchant model omments

--- models/merchantModel.js
+++ models/merchantModel.js
@@ -5,39 +5,48 @@ const MerchantSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	//taprobana ID
 	fullName: {
 		type: String,
 		required: true,
 	},
+	//fullname of the merchnat
 	mobileno: {
 		type: String,
 		required: true,
 	},
+	//mobile number of the merchant
 	email: {
 		type: String,
 		required: true,
 		unique: true,
 	},
+	//email of the merchant
     dateOfBirth: {
 		type: String,
 		required: true,
 	},
+	//date of birth of the merchant
 	address: {
 		type: String,
 		required: true,
 	},
+	//address of the merchant
 	status: {
 		type: String,
         default:null
 	},
+	//status of the merchant
     password: {
 		type: String,
 		required: true,
 	}, 
+	//password of the merchant
 	userRole: {
 		type: String,
 		default: "merchant",
 	},
+	//user role of the merchant
 },
 {
     timestamps: true,
