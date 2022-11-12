8b2e6e26e029aa4bc3db880a76320cfdb738326e
NethmiPrabodhika <prabodhikanethmi@gmail.com>
2022-11-12 21:29:02 +0530

    item model

--- models/ItemsModel.js
+++ models/ItemsModel.js
@@ -5,32 +5,39 @@ const ItemSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	//item id
 	name: {
 		type: String,
 		required: true,
 	},
+	//name of the item
 	description: {
 		type: String,
 		required: true,
 	},
+	//description of the item
 	is_wholesale: {
 		type: Boolean,
 		required: true,
         default: 0
 	},
+	//is_wholesale of the item
 	unit_price: {
 		type: String,
 		required: true,
 	},
+	//unit_price of the item
 	image_url: {
 		type: String,
 		default: null
 	},
+	//image_url of the item
 	owner : {
         type: mongoose.Schema.Types.ObjectId,
 		required: true,
         ref: "merchant"
 	}
+	//owner of the item
 
 },
 	{
